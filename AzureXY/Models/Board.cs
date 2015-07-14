3072772822e11454bb1c2ab84554fc3d270807c4
Teemu Vartiainen <teemu.vartiainen@outlook.com>
2015-07-14 15:20:17 +0300

    Add initial models

---
+++ AzureXY/Models/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AzureXY.Models
+{
+    public class Board
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string AccessToken { get; set; }
+        
+        public List<Drawing> Drawings { get; set; }
+    }
+}
---
+++ AzureXY/Models/Drawing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AzureXY.Models
+{
+    public class Drawing
+    {
+        public int ID { get; set; }
+
+        public string Instructions { get; set; }
+
+        public bool Queued { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public Board Board { get; set; }
+        
+        public Drawing()
+        {
+            Created = DateTime.Now;
+        }
+    }
+}
