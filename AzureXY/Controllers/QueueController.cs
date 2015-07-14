2aee16f9da90ab31bdacba5a14cd2578397beb64
Teemu Vartiainen <teemu.vartiainen@outlook.com>
2015-07-14 16:00:33 +0300

    Add controller stubs

---
+++ AzureXY/Controllers/QueueController.cs
@@ -0,0 +1,37 @@
+﻿using AzureXY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureXY.Controllers
+{
+    public class QueueController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Queue
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Queue/Fetch/5
+        public async Task<ActionResult> Fetch(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = await db.Boards.FindAsync(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+    }
+}
---
+++ AzureXY/Controllers/TablesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureXY.Controllers
+{
+    // this is the public side
+    public class TablesController : Controller
+    {
+        // GET: Tables
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
