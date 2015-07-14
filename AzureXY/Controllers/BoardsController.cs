cfc09a7d87dea1a1bdfaca634c850165ee3bcc9c
Teemu Vartiainen <teemu.vartiainen@outlook.com>
2015-07-14 15:25:52 +0300

    Scaffold BoardsController

---
+++ AzureXY/Controllers/BoardsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AzureXY.Models;
+
+namespace AzureXY.Controllers
+{
+    public class BoardsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Boards
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Boards.ToListAsync());
+        }
+
+        // GET: Boards/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = await db.Boards.FindAsync(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View(board);
+        }
+
+        // GET: Boards/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Boards/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Name,AccessToken")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Boards.Add(board);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(board);
+        }
+
+        // GET: Boards/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = await db.Boards.FindAsync(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Boards/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Name,AccessToken")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(board).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(board);
+        }
+
+        // GET: Boards/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Board board = await db.Boards.FindAsync(id);
+            if (board == null)
+            {
+                return HttpNotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Boards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Board board = await db.Boards.FindAsync(id);
+            db.Boards.Remove(board);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
