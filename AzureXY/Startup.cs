22600036d2120be978cf64e9f796bcd1b7932027
Teemu Vartiainen <teemu.vartiainen@outlook.com>
2015-07-14 15:20:05 +0300

    Add MVC solution

---
+++ AzureXY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureXY.Startup))]
+namespace AzureXY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
