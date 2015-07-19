49374fe35e87002524ad96fde6e5c90351365fd8
Teemu Vartiainen <teemu.vartiainen@outlook.com>
2015-07-19 17:28:31 +0300

    Editor JS stubs

---
+++ AzureXY/Scripts/editor.js
@@ -0,0 +1,36 @@
+﻿
+function Editor(element) {
+    this.canvas = $(element);
+    this.target = this.canvas.attr("data-target");
+    this.lines = [];
+    this.locateButtons();
+    this.registerButtons();
+    this.registerTouch();    
+};
+
+Editor.prototype = {
+    "locateButtons": function () {
+        
+    },
+    "registerButtons": function () {
+
+    },
+    "registerTouch": function () {
+
+    },
+    "actionNewline": function () {
+
+    },
+    "actionDraw": function () {
+
+    },
+    "actionErase": function () {
+
+    },
+};
+
+$(function () {
+    $("canvas.toolbox-canvas").each(function (i, element) {
+        var e = new Editor(element);
+    });
+})
